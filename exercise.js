da27a7514b376de84e3db7371c3f6e40aa9a1c71
Delbertjoo <dsjoo@mit.edu>
2018-04-09 10:05:16 -0400

    exercises updated

--- exercise.js
+++ exercise.js
@@ -26,9 +26,20 @@ var calculateCentroid = function(points){
     //----------------------------------
     //  YOUR CODE
     //----------------------------------    
-
     // return centroid with the following format
-    // return {x:10, y:30};  
+    // return {x:10, y:30}; 
+    var length = points.length;
+    var xSum = 0;
+    var ySum = 0;
+
+    points.forEach(function(point){
+        xSum += point.x;
+        ySum += point.y;
+    });
+
+    var xCentroid = xSum/length;
+    var yCentroid = ySum/length;
+    return {x:Math.floor(xCentroid), y:Math.floor(yCentroid)};
 };
 
 
